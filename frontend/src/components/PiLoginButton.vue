8eb1a8a3c97a4f0832d3ca1cfed12b56f21299df
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 10:26:20 -0500

    Update PiLoginButton.vue

--- frontend/src/components/PiLoginButton.vue
+++ frontend/src/components/PiLoginButton.vue
@@ -1,1 +1,13 @@
+<template>
+  <button @click="login" class="bg-purple-600 text-white px-4 py-2 rounded shadow hover:bg-purple-700">
+    Login with Pi
+  </button>
+</template>
 
+<script setup>
+import { loginWithPi } from '../utils/pi.js';
+
+function login() {
+  loginWithPi();
+}
+</script>
