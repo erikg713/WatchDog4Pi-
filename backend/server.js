fd6f2af7a70ba0978ef1c39a753218d1ea9d7051
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 10:00:07 -0500

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config.js';
+import authRoutes from './routes/authRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
+import fraudRoutes from './routes/fraudRoutes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/fraud', fraudRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server runnin on ${PORT}`));
