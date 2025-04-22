ec365c03fab542af8efd711e968f922dea296f96
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 16:06:24 -0500

    Update AuthModal.vue

--- frontend/src/components/AuthModal.vue
+++ frontend/src/components/AuthModal.vue
@@ -1,1 +1,23 @@
+<template>
+  <div v-if="show" class="modal">
+    <h2>Login with Pi Network</h2>
+    <button @click="login">Login</button>
+    <button @click="$emit('close')">Cancel</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { authenticatePi } from '@/utils/pi.js'
+
+const props = defineProps({ show: Boolean })
+const emit = defineEmits(['close', 'success'])
+
+function login() {
+  authenticatePi(
+    user => emit('success', user),
+    err => alert('Auth failed: ' + err.message)
+  )
+}
+</script>
 
