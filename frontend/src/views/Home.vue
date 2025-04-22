6d8647ec16c30b916f9e7c4cea5ad2b020a6b0ee
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 10:28:01 -0500

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,1 +1,10 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+    <h1 class="text-4xl mb-6">PiGuard</h1>
+    <PiLoginButton />
+  </div>
+</template>
 
+<script setup>
+import PiLoginButton from '../components/PiLoginButton.vue';
+</script>
