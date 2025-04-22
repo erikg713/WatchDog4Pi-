1dc51fac43260fdaaf520b9d41d0f75b488bb3cd
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 10:14:25 -0500

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,8 @@
+import express from 'express';
+import { verifyPiUser } from '../controllers/authController.js';
 
+const router = express.Router();
+
+router.post('/verify', verifyPiUser); // POST /api/auth/verify
+
+export default router;
