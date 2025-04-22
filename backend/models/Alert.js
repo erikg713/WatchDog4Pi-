90576c50ce42683d7cc6029cad0daad192d7557a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 16:10:02 -0500

    Update Alert.js

--- backend/models/Alert.js
+++ backend/models/Alert.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose')
+const AlertSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  transaction: { type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' },
+  message: String,
+  createdAt: { type: Date, default: Date.now }
+})
+module.exports = mongoose.model('Alert', AlertSchema)
 
