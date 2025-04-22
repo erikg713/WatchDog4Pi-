e43be10aae3f62ed8cc409808da08302c9837815
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 16:07:08 -0500

    Update TransactionCard.vue

--- frontend/src/components/TransactionCard.vue
+++ frontend/src/components/TransactionCard.vue
@@ -1,1 +1,13 @@
+<template>
+  <div class="card">
+    <h3>Transaction #{{ transaction._id }}</h3>
+    <p>Amount: {{ transaction.amount }} Pi</p>
+    <p>Status: {{ transaction.status }}</p>
+    <p>Memo: {{ transaction.memo }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({ transaction: Object })
+</script>
 
