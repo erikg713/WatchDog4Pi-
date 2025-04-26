982e7da7f8c94c937cb417456f4e347d3790fa9c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-25 19:58:15 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,12 @@
+// config.js
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err);
+    console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
